94d9b86a5d6370d704d10e9fda4813dda62350ab
Antony Jones <aj@desirableobjects.co.uk>
2019-10-25 16:47:01 +0100

    add tests

--- index.js
+++ index.js
@@ -2,20 +2,16 @@
 
 const PureImage = require('pureimage')
 const Chromatism = require('chromatism')
-const { promisify } = require('util')
 const { join } = require('path')
 const { measureText } = require('pureimage/src/text')
 
 const fontName = 'cabin-bold'
 const Font = PureImage.registerFont(join(__dirname, 'font', 'cabin-bold.ttf'), fontName)
-// const loadFont = promisify(Font.load)
 
-module.exports = async function createAvatar ({ firstName, lastName }, stream) {
-  const size = 128
-  const background = '#'+Math.floor(Math.random()*16777215).toString(16)
+module.exports = async function createAvatar ({ firstName, lastName }, stream, options = {}) {
+  const size = options.size || 128
+  const background = options.background || '#'+Math.floor(Math.random()*16777215).toString(16)
   const { hex: foreground } = Chromatism.contrastRatio(background)
-
-  console.log(background, foreground)
 
   const img = PureImage.make(size, size)
   const ctx = img.getContext('2d')
@@ -29,7 +25,6 @@ module.exports = async function createAvatar ({ firstName, lastName }, stream) {
       ctx.fillStyle = foreground
       const t = `${firstName[0]}${lastName[0]}`
       const { width } = measureText(ctx, t)
-      console.log(width)
       ctx.fillText(t, (size / 2) - width / 2, size / 1.4)
 
       resolve(PureImage.encodeJPEGToStream(img, stream))
--- index.spec.js
+++ index.spec.js
@@ -2,17 +2,28 @@
 
 const createAvatar = require('.')
 const { join } = require('path')
-const { mkdtempSync, createWriteStream } = require('fs')
+const { mkdtempSync, createWriteStream, readFileSync } = require('fs')
 const { tmpdir } = require('os')
+const crypto = require('crypto')
+const { expect } = require('@hapi/code')
 
 describe('#createAvatar()', () => {
+  let outputFile
+
   beforeEach(async () => {
     const dir = mkdtempSync(join(tmpdir(), 'byia-'))
-    const output = createWriteStream(join(dir, 'image.jpg'))
-    await createAvatar({ firstName: 'Antony', lastName: 'MacKenzie-Jones' }, output)
+    outputFile = join(dir, 'image.jpg')
+    const output = createWriteStream(outputFile)
+    await createAvatar({ firstName: 'Antony', lastName: 'MacKenzie-Jones' }, output, { background: '#ff0c7e' })
   })
 
   it('writes correct image', () => {
-    console.log('hi')
+    const data = readFileSync(outputFile, { encoding: 'base64' })
+    const hash = crypto
+      .createHash('md5')
+      .update(data)
+      .digest('hex')
+
+    expect(hash).to.equal('dcd5bed4310b6e099700122aa9cb5112')
   })
 })
